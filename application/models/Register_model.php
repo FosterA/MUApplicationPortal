ff7f429a3f49d1ec341ba18cf683c31817641086
Seanmichael Stanley <SeanmichaelStanley@Seanmichaels-MacBook-Pro.local>
2015-04-16 13:41:32 -0500

    Added login credential check and redirect to homepage

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -3,8 +3,33 @@
 		public function __construct(){
 			parent::__construct();
 			$this->load->database();
-			//$this->db->query("use test");
+			$this->db->query("use test");
 		}
+
+		public function validate(){
+			$profession = $this->input->post('profession');
+
+			if ($profession == 'admin'){
+				$this->db->where('admin_id', $this->input->post('username'));
+				$this->db->where('password', md5($this->input->post('password')));
+				$query = $this->db->get('admin');
+			}
+			else if ($profession == 'instructor'){
+				$this->db->where('instructor_id', $this->input->post('username'));
+				$this->db->where('password', md5($this->input->post('password')));
+				$query = $this->db->get('instructor');	
+			}
+			else{
+				$this->db->where('student_id', $this->input->post('username'));
+				$this->db->where('password', md5($this->input->post('password')));
+				$query = $this->db->get('student');
+			}
+
+			if ($query->num_rows() == 1){
+				return true;
+			}
+		}
+
 		public function insert($array){
 			$sql="insert into student values(?,?,?)";
 			$data[0]=$array['username'];
@@ -14,12 +39,14 @@
 			if($bol)
 				return true;
 		}
+
 		public function showData(){
 			$sql="selelct * from student";
 			$res=$this->db->query($sql);
 			$array=$res->result_array();
 			return $array;
 		}
+
 		public function ajxCheck($id){
 			$sql="select * from student where student_id=?";
 			$data[0]=$id;
