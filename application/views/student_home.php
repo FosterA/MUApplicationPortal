2a97e5b0f710ffb48ee290d02da95ecb6535e228
Seanmichael Stanley <SeanmichaelStanley@user171.embarqnow.net>
2015-04-17 17:25:12 -0500

    Added link to application form to student_home. Added confirmation message that registration was successful.

--- application/views/student_home.php
+++ application/views/student_home.php
@@ -6,7 +6,7 @@
 			<div id="content">
 				<div>
 					<h2>Welcome <?php echo $user?></h2>
-					<p>Pellentesque pede. Donec pulvinar ullamcorper metus. In eu odio at lectus pulvinar mollis. Vestibulum sem magna, elementum ut, vestibulum eu, facilisis quis, arcu. Mauris a dolor. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed blandit. Phasellus pellentesque, ante nec iaculis dapibus, eros justo auctor lectus, a lobortis lorem mauris quis nunc. Praesent pellentesque facilisis elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. In hac habitasse platea dictumst. Mauris a dolor. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+					<p><a href="<?=base_url('index.php/form/index')?>" class="button1">Begin Application</a></p>
 					<p><a href="#" class="button1">Learn More</a></p>
 				</div>
 			</div>
