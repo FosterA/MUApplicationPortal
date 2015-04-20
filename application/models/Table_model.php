7d16a60f0cf29d5c6b67e9d4b0e0b9c3c90b44a7
Liu,Qintai <qltf8@mail.missouri.edu>
2015-04-20 16:31:55 -0500

    get data when admin want to see the detail information about student and add several views in mysql

--- application/models/Table_model.php
+++ application/models/Table_model.php
@@ -13,9 +13,48 @@
 		public function getStatus($offset,$per_page){
 			$data[0]=$offset;
 			$data[1]=$per_page;
-			$sql='select * from status limit ?,?';
+			$sql='select * from statusname limit ?,?';
 			$res=$this->db->query($sql,$data);
 			return $res;
+		}
+		public function getInfor($id){
+			$sql="select nation,degree from status where student_id=?";
+			$data[0]=$id;
+			$res=$this->db->query($sql,$id);
+			$array=$res->result_array();
+			$nation=$array[0]['nation'];
+			$degree=$array[0]['degree'];
+			if($nation=='international'){
+				if($degree=='undergraduate'){
+					$sql="select * from interunder where student_id=?";
+					$res=$this->db->query($sql,$id);
+					$data['res']=$res->result_array();
+				}else{
+					$sql="select * from intergra where student_id=?";
+					$res=$this->db->query($sql,$id);
+					$data['res']=$res->result_array();
+				}
+			}else{
+				if($degree=='undergraduate'){
+					$sql="select * from nativeunder where student_id=?";
+					$res=$this->db->query($sql,$id);
+					$data['res']=$res->result_array();
+				}else{
+					$sql="select * from nativegra where student_id=?";
+					$res=$this->db->query($sql,$id);
+					$data['res']=$res->result_array();
+				}
+			}
+			$sql="select * from curTeach where student_id=?";
+			$res=$this->db->query($sql,$id);
+			$data['curTeach']=$res->result_array();
+			$sql="select * from preTeach where student_id=?";
+			$res=$this->db->query($sql,$id);
+			$data['preTeach']=$res->result_array();
+			$sql="select * from likeTeach where student_id=?";
+			$res=$this->db->query($sql,$id);
+			$data['likeTeach']=$res->result_array();
+			return $data;
 		}
 	}
 ?>
--- application/views/table.php
+++ application/views/table.php
@@ -7,21 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <style>
 @import url('http://fonts.googleapis.com/css?family=Amarante');
 
-html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-  outline: none;
-  -webkit-font-smoothing: antialiased;
-  -webkit-text-size-adjust: 100%;
-  -ms-text-size-adjust: 100%;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-}
+
 html { overflow-y: scroll; }
 body { 
   background: #eee url('http://i.imgur.com/eeQeRmk.png'); /* http://subtlepatterns.com/weave/ */
@@ -33,30 +19,16 @@ body {
   padding-bottom: 55px;
 }
 
-::selection { background: #5f74a0; color: #fff; }
-::-moz-selection { background: #5f74a0; color: #fff; }
-::-webkit-selection { background: #5f74a0; color: #fff; }
 
 br { display: block; line-height: 1.6em; } 
 
 
 
-input, textarea { 
-  -webkit-font-smoothing: antialiased;
-  -webkit-text-size-adjust: 100%;
-  -ms-text-size-adjust: 100%;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-  outline: none; 
-}
 
-blockquote, q { quotes: none; }
-blockquote:before, blockquote:after, q:before, q:after { content: ''; content: none; }
 strong, b { font-weight: bold; } 
 
-table { border-collapse: collapse; border-spacing: 0; }
-img { border: 0; max-width: 100%; }
+table,tr { border-collapse: collapse; border-spacing: 0; border:2px inset #24A198; }
+
 
 h1 { 
   font-family: 'Amarante', Tahoma, sans-serif;
@@ -69,14 +41,6 @@ h1 {
 
 
 /** page structure **/
-#wrapper {
-  display: block;
-  width: 850px;
-  background: #fff;
-  margin: 0 auto;
-  padding: 10px 17px;
-  -webkit-box-shadow: 2px 2px 3px -1px rgba(0,0,0,0.35);
-}
 
 #keywords {
   margin: 0 auto;
@@ -84,11 +48,6 @@ h1 {
   margin-bottom: 15px;
 }
 
-
-#keywords thead {
-  cursor: pointer;
-  background: #c9dff0;
-}
 #keywords thead tr th { 
   font-weight: bold;
   padding: 12px 30px;
@@ -100,8 +59,8 @@ h1 {
   background-position: 100% 100%;
 }
 
-#keywords thead tr th.headerSortUp, #keywords thead tr th.headerSortDown {
-  background: #acc8dd;
+#keywords thead tr {
+  background: #1B9930;
 }
 
 #keywords thead tr th.headerSortUp span {
@@ -122,6 +81,16 @@ h1 {
 #keywords tbody tr td.lalign {
   text-align: left;
 }
+
+#links{
+
+}
+
+#current{
+
+}
+
+
 </style>
 </head>
 <body>
@@ -129,21 +98,20 @@ h1 {
 	<?=$links?>
 	
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
-<script src="http://tablesorter.com/__jquery.tablesorter.min.js" type="text/javascript"></script>
 <script>
 $(document).ready(function(){
 
     var tr = document.getElementsByTagName('tr');
     for(var i=1;i<tr.length;i++){
     	var str=tr[i].children[1].innerHTML;
-    	var a1=tr[i].children[4].children[0];
-    	a1.href="<?=base_url('index.php/processor')?>"+'/'+str;
+    	var a1=tr[i].children[6].children[0];
+    	a1.href="<?=base_url('index.php/table/test')?>"+'/'+str;
     }
 	
+   
+
 });
-$(function(){
-  $('#keywords').tablesorter(); 
-});
+
 </script>
 </script>
 </body>
