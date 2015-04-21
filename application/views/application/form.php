cb156404e8463fef0d0ced6c37bb1ec647afab66
Decong Du <ddbmf@mail.missouri.edu>
2015-04-20 22:01:15 -0500

    Add a menu for selecting date using Jquery.

--- application/views/application/form.php
+++ application/views/application/form.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title>Application Form</title>
+
 <style>
 	
 	.chosen-select:hover{
@@ -14,41 +15,44 @@
   		height:200px;
   		margin:-100px 407px;
   	}
+.calender{ width:319px;margin:50px auto;top:0;left:0;border:4px #D6D6D6 solid;background:#EBEBEB;position:absolute;display:none;z-index:999;}
+.calendertb{width:100%;}
+.calendertb td{width:35px;height:35px;border:1px #CCCCCC solid;text-align:center;vertical-align:middle;cursor:pointer;font-size:14px;font-weight:bold;}
+.calendertb td.hover,.calendertb td.weekendhover{background:#D6D6D6;}
+.calendertb th{width:35px;height:30px;border:1px #CCCCCC solid;text-align:center;vertical-align:middle;cursor:pointer;color:#979797;}
+.tdtoday{ background:#0080FF;color:#fff;width:35px;height:35px;border:1px #CCCCCC solid;text-align:center;vertical-align:middle;cursor:pointer;font-size:14px;font-weight:bold;}
+.getyear{ height:35px;line-height:35px;width:100%;text-align:center;}
+.preMonth{ font-size:14px;font-weight:bold;cursor:pointer;margin-right:18px;color:#0080FF;}
+.nextMonth{ font-size:14px;font-weight:bold;cursor:pointer;margin-left:18px;color:#0080FF;}
+.mh_date{width:319px;height:30px;line-height:20px;padding:5px;cursor:pointer;background:white url("<?=base_url('images/dateIco.png')?>") no-repeat right center;}
+.zhezhao{width:100%;height:100%;position:fixed;z-index:998;background:#fff;filter:alpha(opacity=10);opacity:0.1;display:none;}
+
+
 
 </style>
 
 <link rel="stylesheet" href="<?=base_url('docsupport/style.css')?>">
 <link rel="stylesheet" href="<?=base_url('docsupport/prism.css')?>">
-<link rel="stylesheet" href="<?=base_url('chosen.css')?>">
+<script  src="<?=base_url('jquery-ui-1.11.2/js/jquery-1.7.2.min.js')?>"></script>
+<script src="<?=base_url('jquery-ui-1.11.2/js/manhuaDate.1.0.js')?>"></script>
 </head>
 <body>
 	<form action="#" method="POST" name="application">
 		<input type="hidden" name="id" value="<?=$this->session->userdata('user')?>">
-		<br><div align="center">TA/PLA Application Form</div><br>
-		<br><left>
-		&nbsp;First Name: &nbsp;
+		First Name: <br>
 		<input type="text" name="fname">
-		&nbsp; &nbsp;
+		<br>
 		
-		Last Name: &nbsp;
+		Last Name: <br>
 		<input type="text" name="lname">
-		<br> <br>
-		&nbsp;Phone Number: &nbsp;
-		<input type="text" name="phone">
-		&nbsp; &nbsp;
-		GPA: &nbsp;
+		<br>
+		
+		GPA:<br>
 		<input type="text" name="gpa">
 		<br>
-		<br>
-
-		&nbsp;Mizzou email address: &nbsp;
-		<input type="text" name="email">
-		<br><br>
-
 		<?php if($degree=='undergraduate'){?>
-		&nbsp;Degree Program:&nbsp;
+		If undergraduate, indicate program and level:<br>
 		<input type="text" name="program">
-		&nbsp;Level:&nbsp;
 		<select name='level'>
   			<option value="freshmen">freshmen</option>
  			<option value="sophomore">sophomore</option>
@@ -57,24 +61,29 @@
 		</select>
 		<br>
 		<?php }else{?>
-		&nbsp;Degree Type:
+		If graduate: <br>
 		<input type='radio' name='position' id='MS' value='MS'> 
 		<label for="MS">MS</label>
-		
+		<br>
 		<input type='radio' name='position' id='PhD' value='PhD'>
 		<label for="PhD">PhD</label>
-		&nbsp;&nbsp;
+		<br>
 		Advisor's Name:
 		<input type="text" name="advisor">
 		<br>
 		<?php }?>
-		
-		<br>&nbsp;Anticipated graduation date:&nbsp;
-		<input type="text" name="gradDate">
+		Phone Number:<br>
+		<input type="text" name="phone">
 		<br>
+		
+		Mizzou email address:<br>
+		<input type="text" name="email">
+		
 
-		<br>&nbsp;Course(s) You Are Currently Teaching: <br>&nbsp;(Select multiple by holding command)<br>
-		&nbsp;
+		<br>Anticipated graduation date:<br>
+ 		<input type="text" class="mh_date" name='gradDate' id='date'>
+		<br>Course(s) You Are Currently Teaching:<br>
+
 		<select name="teaching[]" style="width:350px;" class="chosen-select" multiple tabindex="6">
         	<?php foreach($course as $val):?>
         		<?php $dept=$val['deptment'];?>
@@ -86,8 +95,7 @@
           	<?php endforeach;?>
           </select>
 
-		<br>&nbsp;Course(s) You Have Previously Taught: <br>&nbsp;(Select multiple by holding command)<br>
-		&nbsp;
+		<br>Course(s) You Have Previously Taught:<br>
 		<select name="taught[]" style="width:350px;" class="chosen-select" multiple tabindex="6">
         	<?php foreach($course as $val):?>
         		<?php $dept=$val['deptment'];?>
@@ -98,8 +106,7 @@
         		 </optgroup>
           	<?php endforeach;?>
           </select>
-		<br>&nbsp;Course(s) You Would Like to Teach: <br>&nbsp;(Select multiple by holding command)<br>
-		&nbsp;
+		<br>Course(s) You Would Like to Teach:<br>
 		<select name="teach[]" style="width:350px;" class="chosen-select" onchange="fun()" id='taught' class="select" multiple tabindex="6">
         	<?php foreach($course as $val):?>
         		<?php $dept=$val['deptment'];?>
@@ -112,23 +119,25 @@
           </select>
 		<br>
 		<div id="show"></div>
-		<br>&nbsp;Other Places You Work:&nbsp;
+		Other Places You Work:<br>
 		<input type="text" name="work">
 		<br>
 		
 		<?php if($nation=='international'){?>
-		<br>&nbsp;SPEAK/OPT score:&nbsp;
+		SPEAK/OPT score, if applicable:<br>
 		<input type="text" name="score">
 		<br>
 		
-		&nbsp;Semester of last test:&nbsp;
+		Semester of last test:<br>
 		<input type="text" name="test">
 		<br>
 		<?php }?>
 		<br>
-		&nbsp;&nbsp;&nbsp;<button type="button" onclick="sub()">submit</button><br><br>
+		<button type="button" onclick="sub()">submit</button>
 	</form>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
+	
+	
+	
     <script type="text/javascript">
    	function fun(){
    		var content=document.getElementById('show');
@@ -175,6 +184,19 @@
 		form.submit();
 		}
 		
+
+
+$(document).ready(function(){
+    $("input.mh_date").manhuaDate({					       
+		Event : "click",//可选				       
+		Left : 0,//弹出时间停靠的左边位置
+		Top : -16,//弹出时间停靠的顶部边位置
+		fuhao : "-",//日期连接符默认为-
+		isTime : false,//是否开启时间值默认为false
+		beginY : 1949,//年份的开始默认为1949
+		endY :2100//年份的结束默认为2049
+	});
+});
   </script>
 </body>
 </html>
