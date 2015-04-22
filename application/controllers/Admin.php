721dfdf12c78197bf41214ef537b3d6cdaf378ef
Seanmichael Stanley <SeanmichaelStanley@Seanmichaels-MacBook-Pro.local>
2015-04-22 07:15:31 -0500

    Trying to get admin_model.php to work

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
-	/*public function index()
+	public function index()
 	{
-
-	}*/
+		$this->load->model('admin_model');
+	}
 
 	public function setwindows(){
 	
