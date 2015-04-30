4b6226b16449dd17bf3c75cb6698f776b1e3014a
Liu,Qintai <qltf8@mail.missouri.edu>
2015-04-29 21:42:28 -0500

    add sesseion

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,7 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
-	
+	public function __construct(){
+		parent::__construct();
+		if (!isset($_SESSION['logged_in'])||$this->session->userdata('profession')!='admin'){
+			redirect('register');
+		}
+	}
 	public function index(){
 		if (!isset($_SESSION['logged_in'])){
 			redirect('register');
