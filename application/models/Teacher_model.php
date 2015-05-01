6b803f1cd566c53715f9d07a2b17d859d5facc2c
Benjamin Rogers <bar5z6@missouri.edu>
2015-04-30 21:15:11 -0500

    Added a new time message to the instructor page.

--- application/models/Teacher_model.php
+++ application/models/Teacher_model.php
@@ -34,7 +34,36 @@ class Teacher_model extends CI_Model{
 		}
 		
 		else{
-			return "The comment period is not open yet. Check back soon.";
+			$sql = "SELECT * FROM windows";
+			$result = $this->db->query($sql);
+			$row = $result->row();
+				$semester = $row->semester;
+				$appOpen = $row->appOpen;
+				$appClose = $row->appClose;
+				$commentOpen = $row->commentOpen;
+				$commentClose = $row->commentClose;
+			$message = "The comment period is not open yet.";
+			$nowQuery = ($this->db->query("SELECT curdate() as now"));
+			$nowrow= $nowQuery->row();
+			$now = $nowrow->now;
+			if($appOpen>$now){
+				$message = $message.nl2br("\r\n");
+				$message = $message.nl2br("\nThe application period will open on ".strval($appOpen)." and close on ".$appClose.".");
+			}
+			else if($appClose>$now){
+				$message = $message.nl2br("\r\n");
+				$message = $message.nl2br("\nThe application period will close on ".strval($appClose).".");
+			}
+
+			if($commentOpen>$now){
+				$message = $message.nl2br("\r\n");
+				$message = $message.nl2br("\nThe comment period will open on ".strval($commentOpen)." and close on ".$commentClose.".");
+			}
+			else if($commentClose>$now){
+				$message = $message.nl2br("\r\n");
+				$message = $message.nl2br("\nThe comment period will close on ".strval($commentClose).".");
+			}
+			return $message;
 		}
 	}
 }
